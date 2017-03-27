73f691291a162990d8e723831c7646b7220423d7
Suraj Jitindar Singh <sjitindarsingh@gmail.com>
2017-03-28 10:47:43 +1100

    mboxd: Update mboxd to implement protocol V2 and add dbus support
    Version 2 of the mbox protocol contains a few changes such as:
     - All sizes are in block size
     - Adds an erase command
     - Adds new response codes
     - Adds new BMC events
     - Open windows commands now take a size directive
    
    Update the mailbox daemon to support version 2 of the protocol which
    includes implementing all of the V2 functionality. Also entirely refactor
    the mboxd.c code to make it more modular improving readability and
    maintainability.
    
    At the same time improve the functionality by adding:
     - Multiple windows in the daemon (still only one active window) to cache
       flash contents
     - Implement a dbus interface to allow interaction with the daemon
     - Handle sigterm and sigint and terminate cleanly
    
    The previous implementation utilised the entire reserved memory region.
    Update the daemon so that on the command line the number of windows and
    the size of each which the reserved memory region will be split into can
    be specified. The reserved memory region is then divided between the
    windows, however there can still only be one "active" window at a time.
    The daemon uses these windows to cache the flash contents meaning the
    flash doesn't have to be copied when the host requests access assuming
    the daemon already has a copy.
    
    A dbus interface is added so that commands can be sent to the daemon to
    control it's operation from the bmc. These include suspending and resuming
    the daemon to synchronise flash access, telling the daemon to point the lpc
    mapping back to flash and telling the daemon when the flash has been
    modified out from under it.
    
    Signed-off-by: Suraj Jitindar Singh <sjitindarsingh@gmail.com>
    Change-Id: I10be01a395c2bec437cf2c825fdd144580b60dbc

--- common.h
+++ common.h
@@ -4,15 +4,18 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *	Unless required by applicable law or agreed to in writing, software
- *	distributed under the License is distributed on an "AS IS" BASIS,
- *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *	See the License for the specific language governing permissions and
- *	limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
+
+#ifndef COMMON_H
+#define COMMON_H
 
 #ifndef PREFIX
 #define PREFIX ""
@@ -23,6 +26,13 @@ enum {
    MBOX_LOG_VERBOSE = 1,
    MBOX_LOG_DEBUG = 2
 } verbosity;
+
+#define MSG_OUT(f_, ...)	do { if (verbosity >= MBOX_LOG_DEBUG) { \
+					mbox_log(LOG_INFO, f_, ##__VA_ARGS__); \
+				} } while (0)
+#define MSG_ERR(f_, ...)	do { if (verbosity >= MBOX_LOG_VERBOSE) { \
+					mbox_log(LOG_ERR, f_, ##__VA_ARGS__); \
+				} } while (0)
 
 void (*mbox_vlog)(int p, const char *fmt, va_list args);
 
@@ -39,4 +49,40 @@ uint32_t get_u32(uint8_t *ptr);
 
 void put_u32(uint8_t *ptr, uint32_t val);
 
+static inline uint32_t align_up(uint32_t val, uint32_t size)
+{
+	return (((val) + (size) - 1) & ~((size) - 1));
+}
+
+static inline uint32_t align_down(uint32_t val, uint32_t size)
+{
+	return ((val) & ~(((size) - 1)));
+}
+
+static inline uint32_t min_u32(uint32_t a, uint32_t b)
+{
+	if (a <= b) {
+		return a;
+	}
+
+	return b;
+}
+
+static inline int log_2(int val)
+{
+	int ret = 0;
+
+	if (val <= 0) {
+		return -1;
+	}
+
+	while (val >>= 1) {
+		ret++;
+	}
+
+	return ret;
+}
+
 char *get_dev_mtd(void);
+
+#endif /* COMMON_H */
