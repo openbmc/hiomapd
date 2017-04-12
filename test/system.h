aa28a7bf1113431b499f727a984a5ae8d17b9f16
Andrew Jeffery <andrew@aj.id.au>
2017-04-12 14:26:00 +0930

    test: Add system interface to define MTD and MBOX characteristics
    The primary entry point to the system abstraction is through its
    ioctl() implementation, which handles ioctl calls for the MTD and LPC
    subsystems. The MBOX portion needs no support as it is driven purely by
    read() and write() calls.
    
    Two helpers are exposed to configure the MTD and LPC subsystems
    respectively.
    
    Change-Id: Id50abdb967f6ea2ece2e3b21288412d276439c36
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

---
+++ test/system.h
@@ -0,0 +1,9 @@
+#ifndef TEST_SYSTEM_H
+#define TEST_SYSTEM_H
+
+#include <stdint.h>
+
+void system_set_reserved_size(uint32_t size);
+void system_set_mtd_sizes(uint32_t size, uint32_t erasesize);
+
+#endif /* TEST_SYSTEM_H */
