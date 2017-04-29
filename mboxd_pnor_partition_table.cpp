f47305c0653fc5658714f92b1c1bd5f88b808908
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-04-29 13:01:49 -0500

    pnor: provide "C" interface to partition table
    Change-Id: I35af6c4c43e9a43f6a21992bfb0c13542a2c8f0d
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ mboxd_pnor_partition_table.cpp
@@ -0,0 +1,52 @@
+#include "mboxd_pnor_partition_table.h"
+#include "mbox.h"
+#include "pnor_partition_table.hpp"
+
+struct vpnor_partition_table
+{
+    openpower::virtual_pnor::partition::Table* table = nullptr;
+};
+
+void vpnor_create_partition_table(struct mbox_context *context)
+{
+    if (context)
+    {
+        if (!context->vpnor)
+        {
+            context->vpnor = new vpnor_partition_table;
+            context->vpnor->table =
+                new openpower::virtual_pnor::partition::Table;
+        }
+    }
+}
+
+size_t vpnor_get_partition_table_size(const struct mbox_context *context)
+{
+    return context && context->vpnor ?
+        context->vpnor->table->size() : 0;
+}
+
+struct pnor_partition_table* vpnor_get_partition_table(
+                                 const struct mbox_context *context)
+{
+    return context && context->vpnor ?
+        context->vpnor->table->generateHostTable() : nullptr;
+}
+
+const struct pnor_partition* vpnor_get_partition(
+                                 const struct mbox_context *context,
+                                 const off_t offset)
+{
+    return context && context->vpnor ?
+        context->vpnor->table->partition(offset) : nullptr;
+}
+
+void vpnor_destroy_partition_table(struct mbox_context *context)
+{
+    if(context && context->vpnor)
+    {
+        delete context->vpnor->table;
+        delete context->vpnor;
+        context->vpnor = nullptr;
+    }
+}
