bb39dd89a1cef080ddcd5d1202f4b0ec745a5fd3
Ratan Gupta <ratagupt@in.ibm.com>
2017-05-05 21:42:53 +0530

    Load the partitions into virtual pnor
    Resolves openbmc/openbmc#1440
    
    Change-Id: I709c410009fb0047d7e59ddd3a681f25e49341a0
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- mboxd_flash_virtual.cpp
+++ mboxd_flash_virtual.cpp
@@ -17,16 +17,27 @@
  *
  */
 
+#include <fcntl.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <syslog.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 extern "C" {
 #include "common.h"
 }
 
+#include "config.h"
 #include "mboxd_flash_virtual.h"
 #include "mboxd_pnor_partition_table.h"
+
+#include <string>
+#include <exception>
+#include <stdexcept>
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem;
 
 /*
  * copy_flash() - Copy data from the virtual pnor into a provided buffer
@@ -38,27 +49,66 @@ extern "C" {
  * Return:      0 on success otherwise negative error code
  */
 int copy_flash(struct mbox_context *context, uint32_t offset, void *mem,
-	       uint32_t size)
+        uint32_t size)
 {
-	int rc = 0;
+    int rc = 0;
 
-	MSG_DBG("Copy virtual pnor to %p for size 0x%.8x from offset 0x%.8x\n",
-		mem, size, offset);
+    MSG_DBG("Copy virtual pnor to %p for size 0x%.8x from offset 0x%.8x\n",
+            mem, size, offset);
+    try {
+        /* The virtual PNOR partition table starts at offset 0 in the virtual
+         * pnor image. Check if host asked for an offset that lies within the
+         * partition table.
+         */
+        size_t sz =
+            vpnor_get_partition_table_size(context) << context->block_size_shift;
+        if (offset < sz) {
+            struct pnor_partition_table* table =
+                vpnor_get_partition_table(context);
+            memcpy(mem,
+                    ((uint8_t *)table) + offset,
+                    min_u32(sz - offset, size));
+            free(table);
+        } else {
+            /* Copy from virtual pnor into the window buffer */
+            auto partition = vpnor_get_partition(context, offset);
+            if (!partition)
+            {
+                std::string msg = "Couldn't get the partition info for offset " +
+                    offset;
+                throw std::runtime_error(msg);
+            }
 
-	/* The virtual PNOR partition table starts at offset 0 in the virtual
-	 * pnor image. Check if host asked for an offset that lies within the
-	 * partition table.
-	 */
-	size_t sz =
-	vpnor_get_partition_table_size(context) << context->block_size_shift;
-	if (offset < sz) {
-		struct pnor_partition_table* table =
-			vpnor_get_partition_table(context);
-		memcpy(mem,
-		       ((uint8_t *)table) + offset,
-		       min_u32(sz - offset, size));
-		free(table);
-	}
+            fs::path partitionFilePath = std::string(PARTITION_FILES_LOC);
+            partitionFilePath /= partition->data.name;
 
-	return rc;
+            auto fd = open(partitionFilePath.c_str(), O_RDONLY);
+            if (fd == -1)
+            {
+                throw std::runtime_error("Couldn't open the partition file");
+            }
+            // if partition size is > then window size
+            size = partition->data.actual > size ? size : partition->data.actual;
+
+            auto mapped_mem = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, offset);
+
+            if (mem == MAP_FAILED)
+            {
+                std::string msg = "Failed to map" + partitionFilePath.string() + ":"
+                    + strerror(errno);
+                close(fd);
+                throw std::runtime_error(msg);
+            }
+            //copy to the reserved memory area
+            memcpy(mem, mapped_mem, size);
+            munmap(mem, size);
+            close(fd);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        MSG_ERR(e.what());
+        rc = -1;
+    }
+    return rc;
 }
