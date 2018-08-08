2c6168ceeba102328568e772dc8271dcf19c780b
Andrew Jeffery <andrew@aj.id.au>
2018-08-08 16:28:50 +0930

    lpc: Drop __cplusplus guard
    C++ concepts should go in C++ files.
    
    Change-Id: I7a0979dda90b7968000e05fdf7029dea748a33cc
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- lpc.h
+++ lpc.h
@@ -4,18 +4,10 @@
 #ifndef LPC_H
 #define LPC_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int init_lpc_dev(struct mbox_context *context);
 void free_lpc_dev(struct mbox_context *context);
 int point_to_flash(struct mbox_context *context);
 int point_to_memory(struct mbox_context *context);
 int reset_lpc(struct mbox_context *context);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* LPC_H */
