da5aede7762ce179f00be8f3ca7caa7cb460e550
Andrew Jeffery <andrew@aj.id.au>
2018-08-08 16:51:44 +0930

    flash: Rename copy_flash to flash_copy
    Change-Id: If449889fca3370e25a65719d2f5c5ec90258d633
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- flash.h
+++ flash.h
@@ -11,7 +11,7 @@
 
 int flash_dev_init(struct mbox_context *context);
 void flash_dev_free(struct mbox_context *context);
-int64_t copy_flash(struct mbox_context *context, uint32_t offset, void *mem,
+int64_t flash_copy(struct mbox_context *context, uint32_t offset, void *mem,
 		   uint32_t size);
 int set_flash_bytemap(struct mbox_context *context, uint32_t offset,
 		      uint32_t count, uint8_t val);
