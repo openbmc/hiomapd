f46f88a69ad27cb3c3e0640e8b0661ce7375d4f4
Andrew Jeffery <andrew@aj.id.au>
2018-08-09 14:04:27 +0930

    flash: Move access rate estimate from transport header
    Change-Id: Ie20b2cadf1a7eea2ea00bc1c134b2dbb1a23a69d
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- flash.h
+++ flash.h
@@ -7,6 +7,9 @@
 #define FLASH_DIRTY	0x00
 #define FLASH_ERASED	0x01
 
+/* Estimate as to how long (milliseconds) it takes to access a MB from flash */
+#define FLASH_ACCESS_MS_PER_MB		8000
+
 #include "mbox.h"
 
 int flash_dev_init(struct mbox_context *context);
