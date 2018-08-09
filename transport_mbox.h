ab50de817a528fce15240b439ab22f6a288fa607
Andrew Jeffery <andrew@aj.id.au>
2018-08-09 14:56:08 +0930

    protocol: Provide abstraction over event notification
    How this works will be transport-dependent. Move the event notification
    helpers into the protocol abstraction and call-back through the
    registered flush handler as necessary.
    
    Change-Id: I29e3a9a9785b92de46a2b2750257fb7f8480a184
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- transport_mbox.h
+++ transport_mbox.h
@@ -7,13 +7,6 @@
 #include "common.h"
 #include "mbox.h"
 
-#define NO_BMC_EVENT			false
-#define SET_BMC_EVENT			true
-
-int set_bmc_events(struct mbox_context *context, uint8_t bmc_event,
-		   bool write_back);
-int clr_bmc_events(struct mbox_context *context, uint8_t bmc_event,
-		   bool write_back);
 int dispatch_mbox(struct mbox_context *context);
 int init_mbox_dev(struct mbox_context *context);
 void free_mbox_dev(struct mbox_context *context);
