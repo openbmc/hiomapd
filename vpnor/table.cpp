0ad1721664423e88f2dadd3d56a6f69e4be65b11
Andrew Geissler <geissonator@yahoo.com>
2021-05-14 14:01:28 -0500

    vpnor: Fix style issues reported by clang tooling
    The files in this commit need to be changed to fix a warning found in
    the latest gcc version. The issue has to do with using the following
    syntax:
    `const decltype(auto)`
    
    First update the files in question to match with the latest clang11
    formatting.
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: If728d370665b42f2ccafea84dbfd50a2ae80c77a

--- vpnor/table.cpp
+++ vpnor/table.cpp
@@ -2,7 +2,8 @@
 // Copyright (C) 2018 IBM Corp.
 #include "config.h"
 
-extern "C" {
+extern "C"
+{
 #include "backend.h"
 #include "common.h"
 #include "mboxd.h"
@@ -14,9 +15,10 @@ extern "C" {
 #include <endian.h>
 #include <syslog.h>
 
+#include <phosphor-logging/elog-errors.hpp>
+
 #include <algorithm>
 #include <fstream>
-#include <phosphor-logging/elog-errors.hpp>
 #include <regex>
 
 namespace openpower
